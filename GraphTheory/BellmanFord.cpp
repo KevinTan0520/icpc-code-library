18fe73c17cf2f9207c40cd2ecee7a927d9e2a93b
KevinTan0520 <kevintan0520@sjtu.edu.cn>
2026-08-21 20:39:25 +0800

    upd: change bellmanford

--- GraphTheory/BellmanFord.cpp
+++ GraphTheory/BellmanFord.cpp
@@ -32,3 +32,10 @@ bool bellman_ford(const vector<vector<Edge>> &e, vector<int> &dis, int n, int st
 int main() {
     return 0;
 }
+
+/*
+解决差分约束问题：
+把每个变量 x_i 视为节点，对于约束 x_i - x_j <= c_k ，从 j 向 i 连长度为 c_k 的有向边
+dis_0 = 0 ，0 向每个节点连权重为 0 的边 
+有负环则无解否则 dis 为一组解
+*/
